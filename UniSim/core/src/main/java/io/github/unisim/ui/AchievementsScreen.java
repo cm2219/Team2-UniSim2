f7e106153a44eb01df9f8b964d068af518367cb3
ApolloCowan <lfz507@york.ac.uk>
2025-01-10 15:28:54 +0000

    Update AchievementsScreen.java

--- UniSim/core/src/main/java/io/github/unisim/ui/AchievementsScreen.java
+++ UniSim/core/src/main/java/io/github/unisim/ui/AchievementsScreen.java
@@ -1,48 +1,89 @@
-package io.github.unisim.building;
+package io.github.unisim.ui;
 
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.math.Vector2;
-import io.github.unisim.Point;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.ScreenUtils;
+import io.github.unisim.GameState;
 
+public class AchievementsScreen implements Screen {
+    private Stage stage;
+    private Table table;
+    private Skin skin;
+    private TextButton backButton;
 
-/**
- * Represents a building that can be placed on the map.
- */
-public class Building {
-  public Texture texture;
-  // we can save memory by storing only the top-left corner and the size of the building.
-  // This works as all buildings are rectangular.
-  public Point location;
-  public Point size;
-  public float textureScale;
-  public Vector2 textureOffset;
-  public boolean flipped;
-  public BuildingType type;
-  public String name;
-  public Integer price;
+    /**
+     * Sets up the achievements screen with a "Back" button and layout.
+     * Initializes the stage, table, and UI components.
+     */
+    public AchievementsScreen() {
+        stage = new Stage();
+        table = new Table();
+        skin = GameState.defaultSkin;
 
-  /**
-   * Create a new building to display in the building menu and place in the world.
+        backButton = new TextButton("Back", skin);
+        backButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
+                GameState.currentScreen = GameState.startScreen;
+            }
+        });
 
-   * @param texture - The image to draw over the space the building occupies
-   * @param textureScale - The scale of the image compared to the source file
-   * @param textureOffset - The offset of the texture in grid tiles
-   * @param location - The (x, y) co-ordinates of the building on the map
-   * @param size - The size (width, height) of the building in map tiles
-   * @param flipped - Whether to render a flipped variant of the building
-   * @param type - The category of building, must be a BuildingType
-   * @param name - The name of the building to display when selected
-   */
-  public Building(Texture texture, float textureScale, Vector2 textureOffset, Point location,
-      Point size, Boolean flipped, BuildingType type, String name, Integer price) {
-    this.texture = texture;
-    this.location = location;
-    this.size = size;
-    this.textureScale = textureScale;
-    this.textureOffset = textureOffset;
-    this.flipped = flipped;
-    this.type = type;
-    this.name = name;
-    this.price = price;
-  }
+        table.setFillParent(true);
+        table.center();
+        table.add(backButton).center().width(200).height(60);
+
+        stage.addActor(table);
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void show() {}
+
+    /**
+     * Renders the screen and updates the stage.
+     *
+     * @param delta Time in seconds since the last frame.
+     */
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(GameState.UISecondaryColour);
+
+        stage.act(delta);
+        stage.draw();
+    }
+
+    /**
+     * Adjusts the layout when the screen size changes.
+     *
+     * @param width  New screen width in pixels.
+     * @param height New screen height in pixels.
+     */
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void hide() {}
+
+    /**
+     * Cleans up resources used by the achievements screen.
+     */
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
 }
