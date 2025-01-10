d45a7c98be99409c38da6eb75ff967304ca61d20
ApolloCowan <lfz507@york.ac.uk>
2025-01-10 14:40:57 +0000

    Update GameState.java

--- UniSim/core/src/main/java/io/github/unisim/GameState.java
+++ UniSim/core/src/main/java/io/github/unisim/GameState.java
@@ -6,30 +6,51 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import io.github.unisim.ui.GameScreen;
+import io.github.unisim.ui.LeaderboardScreen;
 import io.github.unisim.ui.SettingsScreen;
 import io.github.unisim.ui.StartMenuScreen;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import io.github.unisim.ui.AchievementsScreen;
+
 
 /**
  * Contains a collection of settings and references that should be available globally.
  */
 public class GameState {
-  public static Color UIPrimaryColour = new Color(0.250f, 0.326f, 0.865f, 1.0f);
-  public static Color UISecondaryColour = new Color(0.722f, 0.646f, 0.953f, 1.0f);
-  public static Skin defaultSkin = new Skin(Gdx.files.internal("ui/uiskin.json"));
-  public static Settings settings = new Settings();
-  public static InputProcessor fullscreenInputProcessor = new FullscreenInputProcessor();
-  public static Screen gameScreen = new GameScreen();
-  public static Screen startScreen = new StartMenuScreen();
-  public static Screen settingScreen = new SettingsScreen();
-  public static Screen currentScreen;
-  // Create an unmodifiable set containing the IDs of all buildable tiles
-  // we use a set to make searching more efficient
-  public static Set<Integer> buildableTiles = Stream.of(
-      14, 15).collect(Collectors.toUnmodifiableSet()
-  );
-  public static boolean paused = true;
-  public static boolean gameOver = false;
+    public static Color UIPrimaryColour = new Color(0.250f, 0.326f, 0.865f, 1.0f);
+    public static Color UISecondaryColour = new Color(0.722f, 0.646f, 0.953f, 1.0f);
+    public static Skin defaultSkin = new Skin(Gdx.files.internal("ui/uiskin.json"));
+    public static Settings settings = new Settings();
+    public static InputProcessor fullscreenInputProcessor = new FullscreenInputProcessor();
+    public static Screen gameScreen = new GameScreen();
+    public static Screen leaderboardScreen = new LeaderboardScreen();
+    public static Screen achievementsScreen = new AchievementsScreen();
+    public static Screen startScreen = new StartMenuScreen();
+    public static Screen settingScreen = new SettingsScreen();
+    public static Screen currentScreen;
+    // Create an unmodifiable set containing the IDs of all buildable tiles
+    // we use a set to make searching more efficient
+    public static Set<Integer> buildableTiles = Stream.of(
+        14, 15).collect(Collectors.toUnmodifiableSet()
+    );
+    public static boolean paused = true;
+    public static boolean gameOver = false;
+    public static int satisfaction = 0; // the initial satisfaction score
+    public static final int MAX_SATISFACTION = 100;
+    /**
+     * update the satisfaction, but only increasing
+     * @param delta increasement
+     */
+    public static void increaseSatisfaction(int delta) {
+        int previousSatisfaction = satisfaction;
+        satisfaction += delta;
+        if (satisfaction > MAX_SATISFACTION) {
+            satisfaction = MAX_SATISFACTION;
+        }
+        System.out.println("Previous Satisfaction: " + previousSatisfaction +
+            ", Delta: " + delta +
+            ", New Satisfaction: " + satisfaction);
+    }
 }
