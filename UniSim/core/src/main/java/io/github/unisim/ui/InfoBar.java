035660070158e28a936ac5de067fd4cfd19fb22c
VidhiC127 <hrg529@york.ac.uk>
2025-01-11 16:30:50 +0000

    Update InfoBar.java - InfoBar

--- UniSim/core/src/main/java/io/github/unisim/ui/InfoBar.java
+++ UniSim/core/src/main/java/io/github/unisim/ui/InfoBar.java
@@ -14,132 +14,153 @@ import io.github.unisim.GameState;
 import io.github.unisim.Timer;
 import io.github.unisim.building.BuildingType;
 import io.github.unisim.world.World;
+import io.github.unisim.PlayerBalance;
 
 /**
  * Create a Title bar with basic info.
  */
 public class InfoBar {
-    private ShapeActor bar;
-    private Table infoTable = new Table();
-    private Table titleTable = new Table();
-    private Table buildingCountersTable = new Table();
-    private Label[] buildingCounterLabels = new Label[4];
-    private Skin skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
-    private Label scoreLabel = new Label("86%", skin);
-    private Label titleLabel = new Label("UniSim", skin);
-    private Label timerLabel;
-    private Texture pauseTexture = new Texture("ui/pause.png");
-    private Texture playTexture = new Texture("ui/play.png");
-    private Image pauseImage = new Image(pauseTexture);
-    private Image playImage = new Image(playTexture);
-    private Timer timer;
-    private Cell<Label> timerLabelCell;
-    private Cell<Label> scoreLabelCell;
-    private Cell<Image> pauseButtonCell;
-    private Cell<Table> buildingCountersTableCell;
-    private Cell[] buildingCounterCells;
-    private World world;
-    /**
-     * Create a new infoBar and draws its' components onto the provided stage.
+  private ShapeActor bar;
+  private Table infoTable = new Table();
+  private Table titleTable = new Table();
+  private Table buildingCountersTable = new Table();
+  private Label[] buildingCounterLabels = new Label[4];
+  private Skin skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
+  private Label scoreLabel = new Label("86%", skin);
+  private Label titleLabel = new Label("UniSim", skin);
+  private Label timerLabel;
+  private Texture pauseTexture = new Texture("ui/pause.png");
+  private Texture playTexture = new Texture("ui/play.png");
+  private Image pauseImage = new Image(pauseTexture);
+  private Image playImage = new Image(playTexture);
+  private Timer timer;
+  private Cell<Label> timerLabelCell;
+  private Cell<Label> scoreLabelCell;
+  private Cell<Image> pauseButtonCell;
+  private Cell<Table> buildingCountersTableCell;
+  private Cell[] buildingCounterCells;
+  private World world;
+  //Implement money Imports
+  private PlayerBalance balance;
+  private Label balanceLabel = new Label(balance.getStrBalance(), skin);
+  private Texture moneyTexture = new Texture("ui/money.png");
+  private Image moneyIcon = new Image(moneyTexture);
+  private Cell<Label> balanceCell;
+  private Cell<Image> moneyIconCell;
 
-     * @param stage - The stage on which to draw the InfoBar.
-     */
-    public InfoBar(Stage stage, Timer timer, World world) {
-        this.timer = timer;
-        this.world = world;
-        buildingCounterCells = new Cell[4];
+  /**
+   * Create a new infoBar and draws its' components onto the provided stage.
 
-        // Building counter table
-        for (int i = 0; i < 4; i++) {
-            buildingCounterLabels[i] = new Label("", skin);
-        }
-        buildingCounterCells[0] = buildingCountersTable.add(buildingCounterLabels[0]);
-        buildingCounterCells[1] = buildingCountersTable.add(buildingCounterLabels[1]);
-        buildingCountersTable.row();
-        buildingCounterCells[2] = buildingCountersTable.add(buildingCounterLabels[2]);
-        buildingCounterCells[3] = buildingCountersTable.add(buildingCounterLabels[3]);
+   * @param stage - The stage on which to draw the InfoBar.
+   */
+  public InfoBar(Stage stage, Timer timer, World world, PlayerBalance balance) {
+    this.timer = timer;
+    this.world = world;
+    this.balance = balance;
+    buildingCounterCells = new Cell[4];
 
-        // Info Table
-        timerLabel = new Label(timer.getRemainingTime(), skin);
-        infoTable.center().center();
-        pauseButtonCell = infoTable.add(playImage).align(Align.center);
-        timerLabelCell = infoTable.add(timerLabel).align(Align.center);
-        scoreLabelCell = infoTable.add(scoreLabel).align(Align.center);
-        buildingCountersTableCell = infoTable.add(buildingCountersTable).expandX().align(Align.right);
-
-        // Pause button
-        pauseImage.addListener(new ClickListener() {
-            @Override
-            public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
-                GameState.paused = true;
-                pauseButtonCell.setActor(playImage);
-            }
-        });
-
-        // Play button
-        playImage.addListener(new ClickListener() {
-            @Override
-            public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
-                GameState.paused = false;
-                pauseButtonCell.setActor(pauseImage);
-            }
-        });
-
-        titleTable.add(titleLabel).expandX().align(Align.center);
-
-        bar = new ShapeActor(GameState.UIPrimaryColour);
-        stage.addActor(bar);
-        stage.addActor(infoTable);
-        stage.addActor(titleTable);
+    // Building counter table
+    for (int i = 0; i < 4; i++) {
+      buildingCounterLabels[i] = new Label("", skin);
     }
+    buildingCounterCells[0] = buildingCountersTable.add(buildingCounterLabels[0]);
+    buildingCounterCells[1] = buildingCountersTable.add(buildingCounterLabels[1]);
+    buildingCountersTable.row();
+    buildingCounterCells[2] = buildingCountersTable.add(buildingCounterLabels[2]);
+    buildingCounterCells[3] = buildingCountersTable.add(buildingCounterLabels[3]);
 
-    /**
-     * Called when the UI needs to be updated, usually on every frame.
-     */
-    public void update() {
-        timerLabel.setText(timer.getRemainingTime());
-        buildingCounterLabels[0].setText("Recreation: "
-            + Integer.toString(world.getBuildingCount(BuildingType.RECREATION)));
-        buildingCounterLabels[1].setText("Learning: "
-            + Integer.toString(world.getBuildingCount(BuildingType.LEARNING)));
-        buildingCounterLabels[2].setText("Eating: "
-            + Integer.toString(world.getBuildingCount(BuildingType.EATING)));
-        buildingCounterLabels[3].setText("Sleeping: "
-            + Integer.toString(world.getBuildingCount(BuildingType.SLEEPING)));
-        pauseButtonCell.setActor(GameState.paused ? playImage : pauseImage);
-    }
+    // Info Table
+    timerLabel = new Label(timer.getRemainingTime(), skin);
+    infoTable.center().center();
+    pauseButtonCell = infoTable.add(playImage).align(Align.center);
+    timerLabelCell = infoTable.add(timerLabel).align(Align.center);
+    scoreLabelCell = infoTable.add(scoreLabel).align(Align.center);
+    buildingCountersTableCell = infoTable.add(buildingCountersTable).expandX().align(Align.right);
 
-    /**
-     * Update the bounds of the background & table actors to fit the new size of the screen.
+    //Player Balance
+    moneyIconCell = infoTable.add(moneyIcon).align(Align.center);
+    balanceCell = infoTable.add(balanceLabel).align(Align.center);
 
-     * @param width - The new width of the screen in pixels.
-     * @param height - The enw height of the screen in pixels.
-     */
-    public void resize(int width, int height) {
-        bar.setBounds(0, height * 0.95f, width, height * 0.05f);
-        infoTable.setBounds(0, height * 0.95f, width, height * 0.05f);
-        titleTable.setBounds(0, height * 0.95f, width, height * 0.05f);
-
-        float counterTableWidth = height * 0.27f;
-        buildingCountersTableCell.width(counterTableWidth).height(height * 0.05f);
-        for (int i = 0; i < 4; i++) {
-            buildingCounterLabels[i].setFontScale(height * 0.0015f);
-            buildingCounterCells[i].width(counterTableWidth * 0.5f).height(height * 0.025f);
-        }
-
-        timerLabel.setFontScale(height * 0.002f);
-        timerLabelCell.width(height * 0.08f).height(height * 0.05f);
-        timerLabelCell.padLeft(height * 0.005f);
-        scoreLabel.setFontScale(height * 0.002f);
-        scoreLabelCell.width(height * 0.04f).height(height * 0.05f);
-        scoreLabelCell.padLeft(Math.min(width, height * 2) * 0.14f);
-        pauseButtonCell.width(height * 0.03f).height(height * 0.03f)
-            .padLeft(height * 0.01f).padRight(height * 0.01f);
-
-        titleLabel.setFontScale(height * 0.003f);
-    }
-
-    public void reset() {
+    // Pause button
+    pauseImage.addListener(new ClickListener() {
+      @Override
+      public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
+        GameState.paused = true;
         pauseButtonCell.setActor(playImage);
+      }
+    });
+
+    // Play button
+    playImage.addListener(new ClickListener() {
+      @Override
+      public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
+        GameState.paused = false;
+        pauseButtonCell.setActor(pauseImage);
+      }
+    });
+
+    titleTable.add(titleLabel).expandX().align(Align.center);
+
+    bar = new ShapeActor(GameState.UIPrimaryColour);
+    stage.addActor(bar);
+    stage.addActor(infoTable);
+    stage.addActor(titleTable);
+  }
+
+  /**
+   * Called when the UI needs to be updated, usually on every frame.
+   */
+  public void update() {
+    timerLabel.setText(timer.getRemainingTime());
+    buildingCounterLabels[0].setText("Recreation: "
+        + Integer.toString(world.getBuildingCount(BuildingType.RECREATION)));
+    buildingCounterLabels[1].setText("Learning: "
+        + Integer.toString(world.getBuildingCount(BuildingType.LEARNING)));
+    buildingCounterLabels[2].setText("Eating: "
+        + Integer.toString(world.getBuildingCount(BuildingType.EATING)));
+    buildingCounterLabels[3].setText("Sleeping: "
+        + Integer.toString(world.getBuildingCount(BuildingType.SLEEPING)));
+    pauseButtonCell.setActor(GameState.paused ? playImage : pauseImage);
+    balanceLabel.setText(balance.getStrBalance());
+  }
+
+  /**
+   * Update the bounds of the background & table actors to fit the new size of the screen.
+
+   * @param width - The new width of the screen in pixels.
+   * @param height - The enw height of the screen in pixels.
+   */
+  public void resize(int width, int height) {
+    bar.setBounds(0, height * 0.95f, width, height * 0.05f);
+    infoTable.setBounds(0, height * 0.95f, width, height * 0.05f);
+    titleTable.setBounds(0, height * 0.95f, width, height * 0.05f);
+
+    float counterTableWidth = height * 0.27f;
+    buildingCountersTableCell.width(counterTableWidth).height(height * 0.05f);
+    for (int i = 0; i < 4; i++) {
+      buildingCounterLabels[i].setFontScale(height * 0.0015f);
+      buildingCounterCells[i].width(counterTableWidth * 0.5f).height(height * 0.025f);
     }
+
+    timerLabel.setFontScale(height * 0.002f);
+    timerLabelCell.width(height * 0.08f).height(height * 0.05f);
+    timerLabelCell.padLeft(height * 0.005f);
+    scoreLabel.setFontScale(height * 0.002f);
+    scoreLabelCell.width(height * 0.04f).height(height * 0.05f);
+    scoreLabelCell.padLeft(Math.min(width, height * 2) * 0.14f);
+    pauseButtonCell.width(height * 0.03f).height(height * 0.03f)
+    .padLeft(height * 0.01f).padRight(height * 0.01f);
+
+    titleLabel.setFontScale(height * 0.003f);
+
+    moneyIconCell.width(height * 0.03f).height(height * 0.03f)
+        .padLeft(height * 0.01f);
+    balanceCell.width(height * 0.08f).height(height * 0.05f)
+        .padLeft(height * 0.005f);
+    balanceLabel.setFontScale(height * 0.002f);
+  }
+
+  public void reset() {
+    pauseButtonCell.setActor(playImage);
+  }
 }
